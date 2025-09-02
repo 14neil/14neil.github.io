c1c88c8243ce3655d6c9badf06e577b9d349d85d
Neil Pichay <69232828+14neil@users.noreply.github.com>
2025-09-02 14:09:51 -0700

    Update freelancer.js

--- js/freelancer.js
+++ js/freelancer.js
@@ -28,8 +28,9 @@ $(function() {
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+    target: '.navbar-fixed-top',
+    offset: 51  // height of the fixed navbar
+});
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
