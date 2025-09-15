c6f34c47d0408a15cbcc545deb230880bd1afad5
Neil Pichay <69232828+14neil@users.noreply.github.com>
2025-09-15 11:15:16 -0700

    Update freelancer.js

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,43 +1,27 @@
-/*!
- * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
+  $('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    var target = $($anchor.attr('href'));
 
-        // Adjust scroll position so sections aren't hidden behind navbar
-        var offset = 70; // adjust if your navbar is taller/shorter
+    // Prevent jumping if target is invalid or already active
+    if (target.length && target.offset().top !== $(window).scrollTop()) {
+      $('html, body').stop().animate({
+        scrollTop: target.offset().top - 50 // adjust offset for navbar height
+      }, 1250, 'easeInOutExpo');
+    }
 
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - offset
-        }, 1500, 'easeInOutExpo');
-
-        event.preventDefault();
-    });
-});
-
-// Floating label headings for the contact form
-$(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-    });
+    event.preventDefault();
+  });
 });
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-    target: '.navbar-fixed-top',
-    offset: 80 // keeps active state accurate with navbar offset
+  target: '.navbar-fixed-top',
+  offset: 51
 });
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+  $('.navbar-toggle:visible').click();
 });
