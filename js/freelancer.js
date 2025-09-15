651f61be9120d44095a40aa80a0c78aa9e106216
Neil Pichay <69232828+14neil@users.noreply.github.com>
2025-09-15 12:29:55 -0700

    Update freelancer.js

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,40 +1,27 @@
-/*!
- * Start Bootstrap - Freelancer Bootstrap Theme
- * Code licensed under the Apache License v2.0.
- */
-
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        var navbarHeight = $('.navbar-fixed-top').outerHeight(); // offset for fixed navbar
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - navbarHeight
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+  $('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    var target = $($anchor.attr('href'));
 
-// Floating label headings for the contact form
-$(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-    });
+    // Prevent jumping if target is invalid or already active
+    if (target.length && target.offset().top !== $(window).scrollTop()) {
+      $('html, body').stop().animate({
+        scrollTop: target.offset().top - 50 // adjust offset for navbar height
+      }, 1250, 'easeInOutExpo');
+    }
+
+    event.preventDefault();
+  });
 });
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-    target: '.navbar-fixed-top',
-    offset: $('.navbar-fixed-top').outerHeight() + 1 // ensures active link highlights correctly
+  target: '.navbar-fixed-top',
+  offset: 51
 });
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
-    if($('.navbar-toggle:visible').length) {
-        $('.navbar-toggle').click();
-    }
+  $('.navbar-toggle:visible').click();
 });
