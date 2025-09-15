e740797fba45e4453e8bc597c1b515d5ba07adaf
Neil Pichay <69232828+14neil@users.noreply.github.com>
2025-09-14 21:54:28 -0700

    Update freelancer.js

--- js/freelancer.js
+++ js/freelancer.js
@@ -4,35 +4,57 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
+(function($) {
+  "use strict";
+
+  // -------------------------------
+  // Smooth scrolling using jQuery Easing
+  // -------------------------------
+  $(function() {
     $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+      var $anchor = $(this);
 
-// Floating label headings for the contact form
-$(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
+      // Adjust for fixed navbar height (70px works well for your site)
+      var navOffset = 70;
+
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - navOffset
+      }, 1500, 'easeInOutExpo');
+
+      event.preventDefault();
+    });
+  });
+
+  // -------------------------------
+  // Floating label headings for forms
+  // (Safe to keep even if you don’t use the contact form)
+  // -------------------------------
+  $(function() {
+    $("body")
+      .on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+      })
+      .on("focus", ".floating-label-form-group", function() {
         $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
+      })
+      .on("blur", ".floating-label-form-group", function() {
         $(this).removeClass("floating-label-form-group-with-focus");
-    });
-});
+      });
+  });
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
+  // -------------------------------
+  // Activate Scrollspy to highlight nav items on scroll
+  // -------------------------------
+  $('body').scrollspy({
     target: '.navbar-fixed-top',
-    offset: 70  // fixes the highlight issue for About/Portfolio sections
-});
+    offset: 70 // same value as navOffset above
+  });
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
+  // -------------------------------
+  // Closes the responsive menu on nav item click
+  // -------------------------------
+  $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
-});
+  });
+
+})(jQuery);
