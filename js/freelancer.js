79baf34f99e6b269d308e00d66c4597ac00a1d70
Neil Pichay <69232828+14neil@users.noreply.github.com>
2025-09-15 10:09:36 -0700

    Update freelancer.js

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,87 +1,38 @@
 /*!
- * Freelancer.js - smooth scroll + scrollspy + fixed navbar offset
+ * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-(function($) {
-  "use strict";
-
-  $(function() {
-    var $body = $('body');
-    var $window = $(window);
-    var $navbar = $('.navbar-fixed-top');
-
-    function navHeight() {
-      return $navbar.outerHeight() || 0;
-    }
-
-    // Initialize scrollspy
-    if ($.fn.scrollspy) {
-      $body.scrollspy({
-        target: '.navbar-fixed-top',
-        offset: navHeight() + 1
-      });
-    }
-
-    // Smooth scrolling for nav links
-    $(document).on('click', '.page-scroll a', function(e) {
-      var $anchor = $(this);
-      var href = $anchor.attr('href');
-
-      if (!href || href.charAt(0) !== '#') return;
-      if (href === '#') { e.preventDefault(); return; }
-
-      var $target = $(href);
-      if (!$target.length) return;
-
-      e.preventDefault();
-
-      var nh = navHeight();
-      var targetTop = Math.max(0, Math.round($target.offset().top - nh + 1));
-      var currentTop = $window.scrollTop();
-
-      // Only scroll if target is not current (threshold 2px)
-      if (Math.abs(currentTop - targetTop) > 2) {
-
-        // Temporarily disable scrollspy to prevent jumps
-        if ($.fn.scrollspy) {
-          $body.removeData('bs.scrollspy'); 
-          $body.scrollspy({ target: '.navbar-fixed-top', offset: nh + 1 });
-        }
-
-        $('html, body').stop(true, true).animate(
-          { scrollTop: targetTop },
-          900,
-          'easeInOutExpo',
-          function() {
-            // Refresh scrollspy after animation
-            if ($.fn.scrollspy) $body.scrollspy('refresh');
-          }
-        );
-      }
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
     });
+});
 
-    // Close responsive menu on nav link click
-    $(document).on('click', '.navbar-collapse ul li a', function() {
-      $('.navbar-toggle:visible').click();
+// Floating label headings for the contact form
+$(function() {
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
     });
+});
 
-    // Navbar shrink effect
-    function checkNavbarShrink() {
-      if ($navbar.offset().top > 50) $navbar.addClass('navbar-shrink');
-      else $navbar.removeClass('navbar-shrink');
-    }
-    $window.on('scroll', checkNavbarShrink);
-    checkNavbarShrink();
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top',
+    offset: 51  // fixes the highlight issue for About/Portfolio sections
+});
 
-    // Recalculate scrollspy on resize
-    var resizeTimer;
-    $window.on('resize', function() {
-      clearTimeout(resizeTimer);
-      resizeTimer = setTimeout(function() {
-        if ($.fn.scrollspy) $body.scrollspy('refresh');
-      }, 150);
-    });
-
-  });
-
-})(jQuery);
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
