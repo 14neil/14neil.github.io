0e56b3799e1f361062351a32d43593c09d5d7778
Neil Pichay <69232828+14neil@users.noreply.github.com>
2025-09-15 11:19:05 -0700

    Update freelancer.js

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,27 +1,31 @@
-// jQuery for page scrolling feature - requires jQuery Easing plugin
+// Smooth scrolling using jQuery Easing
 $(function() {
-  $('a.page-scroll').bind('click', function(event) {
+  $('a.page-scroll').on('click', function(event) {
     var $anchor = $(this);
     var target = $($anchor.attr('href'));
 
-    // Prevent jumping if target is invalid or already active
+    // Only animate if the target exists and it's not already in view
     if (target.length && target.offset().top !== $(window).scrollTop()) {
-      $('html, body').stop().animate({
-        scrollTop: target.offset().top - 50 // adjust offset for navbar height
-      }, 1250, 'easeInOutExpo');
+      $('html, body').stop().animate(
+        {
+          scrollTop: target.offset().top - 50 // offset for fixed navbar
+        },
+        1250,
+        'easeInOutExpo'
+      );
     }
 
     event.preventDefault();
   });
 });
 
-// Highlight the top nav as scrolling occurs
+// Activate scrollspy to highlight nav items on scroll
 $('body').scrollspy({
   target: '.navbar-fixed-top',
   offset: 51
 });
 
-// Closes the Responsive Menu on Menu Item Click
+// Close the responsive menu when a menu item is clicked
 $('.navbar-collapse ul li a').click(function() {
   $('.navbar-toggle:visible').click();
 });
