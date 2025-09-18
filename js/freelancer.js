b646cc9fefa5da55afb5d9b6b906d04cfd122550
Neil Pichay <69232828+14neil@users.noreply.github.com>
2025-09-17 20:44:31 -0700

    Update freelancer.js

--- js/freelancer.js
+++ js/freelancer.js
@@ -6,45 +6,32 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-  // This selector correctly targets the 'a' tags inside the 'li' elements with the 'page-scroll' class
-  $('.page-scroll a').bind('click', function(event) {
-    var $anchor = $(this);
-    var target = $($anchor.attr('href'));
-
-    // This line performs the smooth scroll animation
-    $('html, body').stop().animate({
-      scrollTop: target.offset().top - 50 // The '- 50' offsets for your fixed navigation bar
-    }, 1250, 'easeInOutExpo');
-
-    // Prevents the default browser jump to the anchor link
-    event.preventDefault();
-  });
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
 });
-
----
 
 // Floating label headings for the contact form
 $(function() {
-  $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-    $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
-  }).on("focus", ".floating-label-form-group", function() {
-    $(this).addClass("floating-label-form-group-with-focus");
-  }).on("blur", ".floating-label-form-group", function() {
-    $(this).removeClass("floating-label-form-group-with-focus");
-  });
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+    });
 });
-
----
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-  target: '.navbar-fixed-top',
-  offset: 51
-});
-
----
+    target: '.navbar-fixed-top'
+})
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
-  $('.navbar-toggle:visible').click();
+    $('.navbar-toggle:visible').click();
 });
