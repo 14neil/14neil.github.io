5bf1d0899343f71928a0e5e8b6ec11b8a982594e
Neil Pichay <69232828+14neil@users.noreply.github.com>
2025-09-17 19:59:20 -0700

    Update freelancer.js

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,31 +1,27 @@
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+  $('.page-scroll a').bind('click', function(event) {
+    var $anchor = $(this);
+    var target = $($anchor.attr('href'));
 
-// Floating label headings for the contact form
-$(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-    });
+    // Prevent jumping if target is invalid or already active
+    if (target.length && target.offset().top !== $(window).scrollTop()) {
+      $('html, body').stop().animate({
+        scrollTop: target.offset().top - 50 // adjust offset for navbar height
+      }, 1250, 'easeInOutExpo');
+    }
+
+    event.preventDefault();
+  });
 });
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
+  target: '.navbar-fixed-top',
+  offset: 51
+});
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+  $('.navbar-toggle:visible').click();
 });
